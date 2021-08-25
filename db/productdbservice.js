75704a1334bef464adcc64ef1334fbc1f5095ad9
vm4music <vm4music@gmail.com>
2021-08-25 14:40:29 +0800

    test

--- db/productdbservice.js
+++ db/productdbservice.js
@@ -21,6 +21,28 @@ module.exports = {
         } catch (error) {
             return ({ message: error })
         }
+    },
+
+    getKeywordProducts : async function(keyword){
+        const rgx = (pattern) => new RegExp(`.*${pattern}.*`);
+        const searchRgx = rgx(keyword);
+        // const userRegex = new RegExp(keyword, 'i')
+        let logg = await Product.find(
+            { 
+                 name: { $regex: searchRgx, $options: "i" } 
+            }
+        );
+
+            console.log(logg)
+            return logg;
+            // let logg = await Product.find({ name: { $regex: keyword, $options: "i" } }, function(err, docs) {
+            //     console.log("Partial Search Begins");
+            //     console.log(docs);
+            //     });
+            //{ $regex: keyword, $options: "i" }
+        // var result = db.collection('AdSchema').find({
+        //     $or: [ {vehicleDescription : { $regex: search.keyWord, $options: 'i' }}, { adDescription: { $regex: search.keyWord, $options: 'i' } } ]
+        // });
     }
 };
 // Add the logic to show the recently seen products on the carousel on home page.
